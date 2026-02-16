f5f463034616ab847665cc249bc9ff3dadde8943
Dishita Bhardwaj <dishitabhardwaj11@gmail.com>
2026-02-16 14:29:55 +0530

    Add files via upload

---
+++ Day-16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int freq[1001] = {0};  
+    for (int i = 0; i < n; i++)
+    {
+        freq[arr[i]]++;
+    }
+
+    for (int i = 0; i < 1001; i++)
+    {
+        if (freq[i] > 0)
+        {
+            printf("%d:%d ", i, freq[i]);
+        }
+    }
+    return 0;
+}
